2a39aa8a77463f3c377a7a85c54ac2258ad4d02e
Purvansh Arora <purvansh.arora@cashify.in>
2022-04-20 15:46:14 +0530

    [ ] defining & calling methods

--- new.py
+++ new.py
@@ -1,14 +1,25 @@
-
 import sys
+
 sys.path.append('/home/purvansh/PycharmProjects/training-project/proj-1')
 
 from other import func
 
-def new_func(x):
+
+def new_func(x, y):
     print(x)
+    print(type(x), type(y))
+    print(y)
+
+
+def return_five():
+    return 5
+
 
 if __name__ == '__main__':
-
-    new_func("Hello World!")
+    new_func(y="Hello World!", x="HI")
 
     func()
+
+    return_five()
+
+    print(type(return_five()))
