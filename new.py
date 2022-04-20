e92928a240d2c36b3e342161cdf34c28c497a544
Purvansh Arora <purvansh.arora@cashify.in>
2022-04-20 13:41:47 +0530

    Tasks before git completed

---
+++ new.py
@@ -0,0 +1,14 @@
+
+import sys
+sys.path.append('/home/purvansh/PycharmProjects/training-project/proj-1')
+
+from other import func
+
+def new_func(x):
+    print(x)
+
+if __name__ == '__main__':
+
+    new_func("Hello World!")
+
+    func()
---
+++ proj-1/other.py
@@ -0,0 +1,7 @@
+
+def func():
+    print('My name is Purvansh.')
+
+
+if __name__ == '__main__':              # This will only run when this file is run directly
+    print("Hello, My Name is Khan.")
