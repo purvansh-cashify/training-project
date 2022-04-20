355a255d672ac93af1e7aec91502b3e93a677f58
Purvansh Arora <purvansh.arora@cashify.in>
2022-04-20 15:59:30 +0530

    args kwargs done

--- new.py
+++ new.py
@@ -15,7 +15,20 @@ def return_five():
     return 5
 
 
+def myfunc(*args):
+    for i in args:
+        print(i)
+
+
+def kwarg_func(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
+
 if __name__ == '__main__':
+
+    print("Methods - \n")
+
     new_func(y="Hello World!", x="HI")
 
     func()
@@ -23,3 +36,11 @@ if __name__ == '__main__':
     return_five()
 
     print(type(return_five()))
+
+    myfunc([1, 2, 3, 4])
+
+    myfunc([1, 2, 3, 4, 5, 6])
+
+    kwarg_func(**{'a': 1, 'b': 1, 'c': 1})
+
+    kwarg_func(**{'a': 1, 'b': 1, 'c': 1, 'd': 2})
