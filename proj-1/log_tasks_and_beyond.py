d535934f559697bb4b9e7980e00be2c9b19678c7
Purvansh Arora <purvansh.arora@cashify.in>
2022-04-24 12:51:02 +0530

    reading a txt file and printing content

--- proj-1/log_tasks_and_beyond.py
+++ proj-1/log_tasks_and_beyond.py
@@ -1,11 +1,13 @@
 import logging
 import numpy as np
 
-logging.basicConfig(filename='test1_log.log', level=logging.DEBUG)  # setting up the logging file and other configurations
+logging.basicConfig(filename='test1_log.log',
+                    level=logging.DEBUG)  # setting up the logging file and other configurations
 
 arr = np.array([1, 2, 3, 4])  # installed third party package and used it
 print(arr.shape)
 logging.debug(arr.shape)
 logging.debug(arr)
 
-
+f = open("test_file.txt", "r")  # reading a file in python
+print(f.read())  # printing its content
